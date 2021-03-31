297641b1beeae3df1a38870473161f0a33910db3
Jorge Saavedra <jsaavedra@montran.com>
2021-03-31 18:53:07 -0500

    My first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import Contador from './components/Contador'
+
 function App() {
   return (
     <div>
       <h1>Hola Mundo React</h1>
+      <Contador/>
     </div>
   );
 }
