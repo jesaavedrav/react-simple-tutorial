b6fd8b08af9d8e1a02dbbdc8ffcea772e678b591
Jorge Saavedra <jsaavedra@montran.com>
2021-04-04 18:44:42 -0500

    Validate form entries

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,39 +1,58 @@
 import React, { Fragment, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const Formulario = () => {
 
-    const [datos, setDatos] = useState({
-        nombre: "",
-        apellido: ""
-    })
+    const { register, formState: { errors }, handleSubmit } = useForm();
 
-    const handleInputChange = (event) => {
-        console.log(event.target.name)
-        setDatos({
-            ...datos,
-            [event.target.name]: event.target.value
-        })
+    const onSubmit = (data, e) => {
+        console.log(data)
+        e.target.reset()
     }
+
 
     return (
         <Fragment>
             <h3>Formulario</h3>
-            <form className="row">
+            <form className="row" onSubmit={handleSubmit(onSubmit)}>
                 <div className="col m-2">
-                    <input placeholder="Nombre" className="form-control" type="text"
-                    name="nombre"
-                    onChange={handleInputChange}/>
+                    <input
+                        placeholder="Nombre"
+                        className="form-control"
+                        type="text"
+                        name="nombre"
+                        {
+                        ...register("nombre", {
+                            required: true
+                        })
+                        } />
+                    <span className="text-danger text-small d-block mb-2">
+                        {errors.nombre && "Nombre es obligatorio"}
+                    </span>
+
+
                 </div>
                 <div className="col m-2">
-                    <input placeholder="Apellido" className="form-control" type="text"
-                    name="apellido"
-                    onChange={handleInputChange}/>
+                    <input
+                        placeholder="Apellido"
+                        className="form-control"
+                        type="text"
+                        name="apellido"
+                        {
+                            ...register("apellido", {
+                                required: true
+                            })
+                            } />
+                        <span className="text-danger text-small d-block mb-2">
+                            {errors.apellido && "Apellido es obligatorio"}
+                        </span>
                 </div>
                 <div className="col m-2">
-                    <button className="btn btn-primary" type="submit">Enviar</button>
+                    <button
+                        className="btn btn-primary"
+                        type="submit">Enviar</button>
                 </div>
             </form>
-            <h3>{datos.nombre} - {datos.apellido}</h3>
         </Fragment>
     );
 }
